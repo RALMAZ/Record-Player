1011ebd1399cf1ccb38a5bb2bfd7662fd7285a1e
Roman Almazov <ralmaz.vue@ya.ru>
2020-04-11 13:45:26 +0300

    update

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,11 +1,5 @@
 import Vue from 'vue'
 
-// const bugsnag = require('@bugsnag/js')
-// const bugsnagVue = require('@bugsnag/plugin-vue')
-// const bug = bugsnag('7748ebd1125f0a74119fe7fad58d256e')
-// bug.use(bugsnagVue, Vue)
-
-if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
 
 import App from './App'
@@ -13,5 +7,4 @@ import App from './App'
 new Vue({
   components: { App },
   template: '<App/>',
-  // bug
 }).$mount('#app')
